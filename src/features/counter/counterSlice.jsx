1509012942e93d05e0f139e02f8c5bf9665f19aa
Ruma303 <matteoh8@hotmail.it>
2023-09-13 10:08:06 +0200

    cambiamento

--- src/features/counter/counterSlice.jsx
+++ src/features/counter/counterSlice.jsx
@@ -1,14 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+    count: 0,
+}
+
 const counterSlice = createSlice({
     name: 'counter',
-    initialState: 0,
+    initialState,
     reducers: {
-        increment: state => state + 1,
-        decrement: state => state - 1,
+        increment: (state) => state.count += 1,
+        decrement: (state) => state.count - 1,
     },
 })
 
-export const { increment, decrement } = counterSlice.actions
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
 
-export default counterSlice.reducer
