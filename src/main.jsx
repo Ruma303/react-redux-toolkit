87c6248e51a2320495bfd243ab298d2aa9901502
Ruma303 <matteoh8@hotmail.it>
2023-09-16 12:55:51 +0200

    aggiunto bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { store } from './store.js';
 import { Provider } from 'react-redux';
 
