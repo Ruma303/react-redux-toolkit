8a301e7c1a3ddfededa0ea04fd745122b70b11b1
Ruma303 <matteoh8@hotmail.it>
2023-09-23 12:28:31 +0200

    aggiornamento da perfettino

--- src/Redux/Features/Middleware/api.js
+++ src/Redux/Features/Middleware/api.js
@@ -1,17 +1,17 @@
-    import axios from "axios";
-    import { fetchDogsStart, fetchDogsSuccess, fetchDogsFailure } from '../functions/dogSlice';
+import axios from "axios";
+import { fetchDogsStart, fetchDogsSuccess, fetchDogsFailure } from '../functions/dogSlice';
 
-    const api = ({ dispatch }) => next => async action => {
-        if (action.type === fetchDogsStart.type) {
-            try {
-                const response = await axios.get('https://dog.ceo/api/breeds/list/all');
-                dispatch(fetchDogsSuccess(response.data.message));
-            } catch (error) {
-                dispatch(fetchDogsFailure(error.message));
-            }
-        } else {
-            next(action);
+const api = ({ dispatch }) => next => async action => {
+    if (action.type === fetchDogsStart.type) {
+        try {
+            const response = await axios.get('https://dog.ceo/api/breeds/list/all');
+            dispatch(fetchDogsSuccess(response.data.message));
+        } catch (error) {
+            dispatch(fetchDogsFailure(error.message));
         }
+    } else {
+        next(action);
     }
+}
 
-    export default api;
+export default api;
