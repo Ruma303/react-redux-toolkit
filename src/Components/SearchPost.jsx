2be10f780c91a7ca20a549b0090fe256c9483f13
Ruma303 <matteoh8@hotmail.it>
2023-09-26 18:06:45 +0200

    SearchPost ok

--- src/Components/SearchPost.jsx
+++ src/Components/SearchPost.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useGetPostQuery } from '../Features/postApi';
 
 const SearchPost = () => {
-  return (
-    <div>SearchPost</div>
-  )
+    const [input, setInput] = useState('');
+    const { data: post, isLoading, isError, error } = useGetPostQuery(input);
+
+    const handleValueChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setInput('');
+    }
+
+    return (
+        <p>
+            <h1>Search Post</h1>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="search mr-3">Search post number: </label>
+                <input
+                    id="search"
+                    type="number"
+                    value={input}
+                    onChange={handleValueChange}
+                />
+            </form>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : isError ? (
+                <p>Error: {error.status}</p>
+            ) : post && input == '' ? (
+                <p>Please, submit a number</p>
+            ) : post ? (
+                <p>{post.id}. {post.title}</p>
+            ) : (
+                <p>No post found.</p>
+            )}
+        </p>
+    );
 }
 
-export default SearchPost
+export default SearchPost;
--- src/Features/postApi.js
+++ src/Features/postApi.js
@@ -38,4 +38,4 @@ export const {
     useCreatePostMutation,
     useUpdatePostMutation,
     useDeletePostMutation,
-} = postApi
+} = postApi;
